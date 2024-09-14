36418cd66ba631ce7b3108d4ac3332eed71c9fac
Spudwick <olivert321@gmail.com>
2024-09-14 17:39:42 +0100

    Copyright headers

--- build-info.cmake
+++ build-info.cmake
@@ -1,3 +1,8 @@
+#=========================================================
+# Copyright (C) 2024 Thomas Oliver - All Rights Reserved
+# You may use, distribute and modify this code under the
+# terms of the GPL license distributed with this code.
+#=========================================================
 
 #=========================================================
 # PRIVATE MEMBERS
