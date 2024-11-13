8f76c807eae443b3ceea36902a454b9b1c1d4a1c
Spudwick <olivert321@gmail.com>
2024-11-13 06:30:04 +0000

    interace lib rather than directly adding header

--- build-info.cmake
+++ build-info.cmake
@@ -130,13 +130,19 @@ function(t_build_target_add_header TARGET HDR_INC_PATH)
             _t_build_always_rebuild
     )
 
-    target_include_directories(${TARGET}
-        PRIVATE ${INC_DIR}
+    set(TARGET_LIB _${TARGET}_buildinfo)
+
+    add_library(${TARGET_LIB} INTERFACE)
+
+    target_sources(${TARGET_LIB} INTERFACE
+        ${HDR_PATH}
     )
 
-    target_sources(${TARGET}
-        PRIVATE ${HDR_PATH}
+    target_include_directories(${TARGET_LIB} INTERFACE
+        ${INC_DIR}
     )
+
+    add_library(${TARGET}::buildinfo ALIAS ${TARGET_LIB})
 
     list(POP_BACK CMAKE_MESSAGE_INDENT)
     message(CHECK_PASS "success")
